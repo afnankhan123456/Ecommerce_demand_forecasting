88f1ca857985bd17ae0dfc441728b3913e0ee397
afnan khan <afnankhan529683@gmail.com>
2026-02-19 16:01:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template
 import numpy as np
 import tensorflow as tf
 
 app = Flask(__name__)
 
-# Load model
 model = tf.keras.models.load_model("demand_forecast_v1.keras")
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "Ecommerce Demand Forecast Model is Live!"
+    prediction = None
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    data = request.json["features"]
-    
-    arr = np.array(data).reshape(1, -1)
-    prediction = model.predict(arr)
-    
-    return jsonify({
-        "prediction": float(prediction[0][0])
-    })
+    if request.method == "POST":
+        features = [
+            float(request.form["price"]),
+            float(request.form["discount_percent"]),
+            float(request.form["ad_spend"]),
+            float(request.form["page_views"]),
+            float(request.form["cart_additions"]),
+            float(request.form["avg_session_time"]),
+            float(request.form["competitor_price"]),
+            float(request.form["seasonality_index"]),
+            float(request.form["day_of_week"])
+        ]
+
+        arr = np.array(features).reshape(1, -1)
+        result = model.predict(arr)
+        prediction = float(result[0][0])
+
+    return render_template("index.html", prediction=prediction)
 
 if __name__ == "__main__":
     app.run()
